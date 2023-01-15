d2b44217b349202159578c29eebd6ce6cb873b18
xCall <carlosfaita99@gmail.com>
2023-01-15 12:53:29 -0300

    configurado carrousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,48 @@
 import Image from 'next/image';
+import {useKeenSlider} from 'keen-slider/react';
 import { HomeContainer, Product } from "../styles/pages/home";
+
+import 'keen-slider/keen-slider.min.css';
 
 import shirt1 from '../assets/shirts/shirt-1.png';
 import shirt2 from '../assets/shirts/shirt-2.png';
 import shirt3 from '../assets/shirts/shirt-3.png';
-import shirt4 from '../assets/shirts/shirt-4.png';
+import shirt4 from '../assets/shirts/Shirt-4.png';
 
 export default function Home() {
+
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  });
+
   return (
-   <HomeContainer>
-    <Product>
+   <HomeContainer ref={sliderRef} className='keen-slider'>
+    <Product className='keen-slider__slide'>
       <Image src={shirt1} width={520} height={480} alt={'Image ilustrator'}/>
       <footer>
         <strong>Camiseta X</strong>
         <span>R$ 79,90</span>
       </footer>
     </Product>
-    <Product>
+    <Product className='keen-slider__slide'>
       <Image src={shirt2} width={520} height={480} alt={'Image ilustrator'}/>
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,90</span>
+      </footer>
+    </Product>
+    <Product className='keen-slider__slide'>
+      <Image src={shirt3} width={520} height={480} alt={'Image ilustrator'}/>
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,90</span>
+      </footer>
+    </Product>
+    <Product className='keen-slider__slide'>
+      <Image src={shirt4} width={520} height={480} alt={'Image ilustrator'}/>
       <footer>
         <strong>Camiseta X</strong>
         <span>R$ 79,90</span>
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -21,6 +21,13 @@ export const {
 
       green500: '#00875F',
       green300: '#00B37E',
+    },
+
+    fontSize: {
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
     }
   }
 });
